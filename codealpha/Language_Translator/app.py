fcb08e319979613d4393f37be1cdef639965fb7a
n.swathi <swathicai08@gmail.com>
2026-07-07 22:39:07 +0530

    Fix app.py for Render

--- codealpha/Language_Translator/app.py
+++ codealpha/Language_Translator/app.py
@@ -1,29 +1,34 @@
 from flask import Flask, render_template, request, jsonify
 from translator import translate_text
+import os
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/translate", methods=["POST"])
 def translate():
     data = request.get_json()
 
-    text = data["text"]
-    source = data["source"]
-    target = data["target"]
+    text = data.get("text")
+    source = data.get("source")
+    target = data.get("target")
 
-    translated = translate_text(
-        text,
-        source,
-        target
-    )
+    try:
+        translated = translate_text(text, source, target)
+        return jsonify({
+            "translated": translated
+        })
+    except Exception as e:
+        return jsonify({
+            "translated": f"Error: {str(e)}"
+        })
 
-    return jsonify({
-        "translated": translated
-    })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
